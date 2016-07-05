f6591c497a09d60f75cae098afa73dbe0946f712
Hans Yulian <hansyulian@windowslive.com>
2016-07-05 15:30:07 +0800

    additional feature for watch

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ var gulpWorker = {};
         combined_postfix: "",
         minified_prefix: "",
         minified_postfix: ".min",
+        automatic_versioning: false,
+        changelog_file_name: "changelog.txt",
         gulp_on_watch: true
     };
     var config = gulpWorker.config;
@@ -25,10 +27,17 @@ var gulpWorker = {};
         uglify_css = require('gulp-uglifycss'),
         sourcemaps = require('gulp-sourcemaps'),
         minify = require("minifier"),
-        fileExists = require("file-exists")
-    colors = require("colors");
+        fileExists = require("file-exists"),
+        colors = require("colors"),
+        debug = require("gulp-debug"),
+        using = require("gulp-using"),
+        filelist = require("gulp-filelist"),
+        less = require("gulp-less");
     var argv = require('yargs').argv,
         version = argv.build ? argv.build : "";
+    var log = require('gulp-util').log;
+
+
 
     var init_settings = function(options) {
         var settings = JSON.parse(JSON.stringify(gulpWorker.config));
@@ -49,13 +58,33 @@ var gulpWorker = {};
         //checking file existence and passing to process array
         for (i in files) {
             var file = directory + files[i];
-            if (fileExists(file)) {
-                console.log("\t\t\t" + colors.green(file));
-                sources.push(file);
-            } else
-                console.log("\t\t\t" + colors.red(file) + " <- Missing!");
+            console.log("\t\t\t" + colors.green(file));
+            sources.push(file);
+            // if (fileExists(file)) {
+            //     console.log("\t\t\t" + colors.green(file));
+            //     sources.push(file);
+            // } else
+            //     console.log("\t\t\t" + colors.red(file) + " <- Missing!");
         }
         return sources;
+    };
+    var automatic_version = function(settings) {
+        var fs = require('fs');
+        var path = require('path');
+
+        var file_path = path.join(__dirname, settings.changelog_file_name);
+        var data = fs.readFileSync(file_path, {
+            encoding: 'utf-8'
+        });
+        var regex = /(\d.\d.\d)/;
+        var match = regex.exec(data);
+        var result = match[0];
+        console.log(colors.green("\tAutomatic versioning, detected version: " + result));
+        if (result)
+            return result;
+        else
+            return "";
+
     }
 
     var worker = function(files, options, type) {
@@ -67,7 +96,10 @@ var gulpWorker = {};
         var sources = filePaths(files, settings.base_folder, {
             show_log: true
         });
-        var name = settings.name + (version != "" ? "-" + version : "");
+
+        if (settings.automatic_versioning && version == "")
+            version = automatic_version(settings);
+        var name = settings.name + (version != "" && settings.version_on_file ? "-" + version : "");
         console.log(colors.yellow("\n\n\t\tProcess Logs: "));
         //determine which uglify going to be used
         var uglify = type == "js" ? uglify_js : uglify_css;
@@ -75,6 +107,11 @@ var gulpWorker = {};
         //initialize source maps if configured so
         if (settings.generate_sourcemaps) {
             process = process.pipe(sourcemaps.init());
+        }
+        //if less, compile it and treat as normal css afterward
+        if (type == "less") {
+            process = process.pipe(less());
+            type = "css";
         }
         //combine files
         var file_name = settings.combined_prefix + name + settings.combined_postfix + "." + type;
@@ -100,7 +137,7 @@ var gulpWorker = {};
         return {
             files: sources
         };
-    }
+    };
 
     gulpWorker.js = function(files, options) {
         works.push({
@@ -108,7 +145,7 @@ var gulpWorker = {};
             options: options,
             type: "js"
         });
-    }
+    };
 
     gulpWorker.css = function(files, options) {
         works.push({
@@ -116,26 +153,41 @@ var gulpWorker = {};
             options: options,
             type: "css"
         });
+    };
+
+    gulpWorker.less = function(files, options) {
+        works.push({
+            files: files,
+            options: options,
+            type: "less"
+        })
     }
 
     gulp.task('watch', function() {
         for (var i = 0; i < works.length; i++) {
-            var work = works[i];
+            var work = JSON.parse(JSON.stringify(works[i]));
             //if the gulp_on_watch set true, when console run gulp watch, the worker will be called once
             var gulp_on_watch = ("gulp_on_watch" in work.options) ? work.settings.gulp_on_watch : gulpWorker.config.gulp_on_watch;
             if (gulp_on_watch)
                 worker(work.files, work.options, work.type);
             //initialize settings
+            var additionals = work.options.additional_watch || [];
+            if (typeof(additionals) == "string")
+                additionals = [additionals];
+            var watches = work.files.concat(additionals);
             console.log(colors.cyan("\tStarting watch:\n"));
             console.log(colors.yellow("\t\tWatching Files:\n"));
             settings = init_settings(work.options);
-            var sources = filePaths(work.files, settings.base_folder, {
+            var sources = filePaths(watches, settings.base_folder, {
                 show_log: true
             });
-
-            gulp.watch(sources, function() {
-                worker(work.files, work.options, work.type);
-            });
+            ! function(s, settings) {
+                gulp.watch(s, function() {
+                    console.log(colors.magenta("Changes detected, starting to rebuild files"));
+                    worker(settings.files, settings.options, settings.type);
+                    console.log(colors.magenta("Rebuild from watch finished\n"));
+                });
+            }(sources, work);
         }
     });
 
@@ -143,5 +195,9 @@ var gulpWorker = {};
         for (var i = 0; i < works.length; i++) {
             var returns = worker(works[i].files, works[i].options, works[i].type);
         }
+    });
+
+    gulp.task("test", function() {
+
     });
 })();
